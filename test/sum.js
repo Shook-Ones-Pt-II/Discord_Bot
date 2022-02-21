74d1bc09d23dc3b8f833032d3990c4ad0f5014d1
Andre Ibrahim <55925084+Andreibr1@users.noreply.github.com>
2022-02-21 15:48:54 -0500

    asfdsf

--- test/sum.js
+++ test/sum.js
@@ -11,6 +11,6 @@ describe('The THREE object', function() {
   it('should be able to construct a Vector3 with default of x=0', function() {
     const vec3 = new THREE.Vector3();
     assert.equal(0, vec3.x);
-    assert.true(true);
+    assertTrue(true);
   })
 })
